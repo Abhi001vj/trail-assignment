2523bf3e519be4db7c6142ea2dcfd03d6805cc12
abhilash_vj <abhilash.vj@iinerds.com>
2021-04-15 15:06:32 +0530

    Added list ed point for sales

--- sale/urls.py
+++ sale/urls.py
@@ -4,19 +4,19 @@ from rest_framework.routers import DefaultRouter
 from sale.views import SaleStatisticsView
 
 router = DefaultRouter(trailing_slash=False)
-# router.register("sales", SaleViewSet, basename="sales")
+router.register("sales", SaleStatisticsView, basename="sales")
 
 urlpatterns = router.urls
 
 
 urlpatterns += [
     path(
-        "sales/",
+        "sales",
         SaleStatisticsView.as_view({"post": "create"}),
         name="sales",
     ),
     path(
-        "sale_statistics/",
+        "sale_statistics",
         SaleStatisticsView.as_view({"get": "get_stats"}),
         name="sale_statistics",
     )
